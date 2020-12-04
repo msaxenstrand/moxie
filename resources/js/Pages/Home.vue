305d297b39bf0bc828ae48eab5c77bd4aba81f34
Mathias Hedström <mathias.hedstrom@haus.se>
2020-12-04 14:04:17 +0100

    Layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,13 +2,18 @@
     <b-container class="py-5">
         <b-row>
             <b-col :key="year" xs="12" md="6" v-for="(docs, year) in documents" class="mb-3">
-                <h4>{{ year }}</h4>
+                <h4 class="ml-2">{{ year }}</h4>
                 <div class="document-wrapper d-flex" v-for="document in docs">
-                    <i class="fas fa-file-alt fa-2x" />
+                  <div class="d-flex">
+                    <i class="fas fa-2x" :class="document.mime_type === 'application/pdf' ? 'fa-file-pdf' : 'fa-file-alt'" />
                     <div class="pl-2">
-                        <a :key="document.id" :href="`/documents/${document.id}`" :download="document.file_name">{{ document.title }}</a>
-                        <div class="font-italic">{{ document.document_type.name }}</div>
+                      <a :key="document.id" :href="`/documents/${document.id}`" :download="document.file_name">{{ document.title }}</a>
+                      <div class="font-italic">{{ document.document_type.name }}</div>
                     </div>
+                  </div>
+                  <a class="download" :key="document.id" :href="`/documents/${document.id}`" :download="document.file_name">
+                    <i class="fas fa-download" />
+                  </a>
                 </div>
             </b-col>
         </b-row>
