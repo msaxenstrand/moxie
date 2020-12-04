36df28ffbc6d79b04b92f2b86ceb9fd42fdcd351
Mathias Hedström <mathias.hedstrom@haus.se>
2020-12-04 11:24:42 +0100

    Added logos

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,7 +7,7 @@
                     <div class="flex">
                         <!-- Logo -->
                         <div class="flex-shrink-0 flex items-center">
-                            <a href="/dashboard">
+                            <a href="/admin/documents">
                                 <jet-application-mark class="block h-9 w-auto" />
                             </a>
                         </div>
